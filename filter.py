6299299c6a516e9be71512703d0835c23f739c1b
Madzena-1 <madzenamukhe1@gmail.com>
2026-04-23 12:31:16 +0200

    Update filter.py

--- filter.py
+++ filter.py
@@ -10,3 +10,31 @@ def filter_active_jobs(jobs):
             active_jobs.append(job)
 
     return active_jobs
+    from datetime import datetime
+from transformers import pipeline
+
+# Load AI model once
+job_classifier = pipeline("text-classification")
+
+def is_job_legit(job):
+    text = job["title"] + " " + job["company"]
+
+    result = job_classifier(text)[0]
+
+    # Very simple rule (we refine later)
+    if result["score"] > 0.5:
+        return True
+    return False
+
+
+def filter_active_jobs(jobs):
+    active_jobs = []
+    today = datetime.today()
+
+    for job in jobs:
+        expiry = datetime.strptime(job["expiry_date"], "%Y-%m-%d")
+
+        if expiry >= today and is_job_legit(job):
+            active_jobs.append(job)
+
+    return active_jobs
